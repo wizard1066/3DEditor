643144ca3936a21e74314f5db457956cfec47226
wizard1066 <mark.lucking@gmail.com>
2022-10-14 10:08:08 +0200

    Initial Commit

---
+++ Molecule/MoleculeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoleculeApp.swift
+//  Molecule
+//
+//  Created by localuser on 14.10.22.
+//
+
+import SwiftUI
+
+@main
+struct MoleculeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
